ca0d2d34beb0875af50e71c4b3a6438a30b5fc62
amruth pai <26.amruth@gmail.com>
2025-10-19 07:09:11 -0500

    Initial commit

---
+++ test/aws_lamda_web_scrapper.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsLamdaWebScrapper from '../lib/aws_lamda_web_scrapper-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws_lamda_web_scrapper-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsLamdaWebScrapper.AwsLamdaWebScrapperStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
