2dbdccfed66bff439926f4e20dfdb92218bbad4d
amruth pai <26.amruth@gmail.com>
2025-10-19 08:56:40 -0500

    basic lambda deployment

--- image/src/main.py
+++ image/src/main.py
@@ -1,3 +1,7 @@
 # lambda function handler 
+import numpy as np
+
 def handler(event, context):
-    return {"satusCode":200, "body":"Hello World!"}
+    arr=np.random.randint(0,10,(3,3))
+    return {"satusCode":200, 
+            "body":{"array":arr.tolist()}}
--- lib/aws_lamda_web_scrapper-stack.ts
+++ lib/aws_lamda_web_scrapper-stack.ts
@@ -1,16 +1,34 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import { ALL } from 'dns';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AwsLamdaWebScrapperStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+  
+    const dockerFunc=new lambda.DockerImageFunction(this, "DockerFunc",{
+      code:lambda.DockerImageCode.fromImageAsset("./image"),
+      memorySize:1024,
+      timeout:cdk.Duration.seconds(10),
+      // architecture: lambda.Architecture.ARM_64,
+    });
 
-    // The code that defines your stack goes here
+    const functionUrl=dockerFunc.addFunctionUrl({
+      authType:lambda.FunctionUrlAuthType.NONE,  //auth users 
+      cors :{
+        allowedMethods: [lambda.HttpMethod.ALL],
+        allowedHeaders:["*"],
+        allowedOrigins:["*"],
+      },
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsLamdaWebScrapperQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new cdk.CfnOutput(this,"FunctionUrlValue",{
+      value: functionUrl.url,
+    })
+
   }
 }
+
+
